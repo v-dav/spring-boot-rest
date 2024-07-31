a16d78e255c56efb5bc7d8a0c2c03dbd4001a32d
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-07-31 12:15:04 +0200

    remove unused imports

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -3,8 +3,6 @@ package com.example.demo.student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 
 @RestController
