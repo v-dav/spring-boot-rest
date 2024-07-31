166de56cfd287815bbd2af3a8fbbec442e83d520
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-07-31 12:13:50 +0200

    created basic REST back end

---
+++ src/main/java/com/example/demo/student/Student.java
@@ -0,0 +1,96 @@
+package com.example.demo.student;
+
+import jakarta.persistence.*;
+import java.time.LocalDate;
+import java.time.Period;
+
+
+@Entity
+@Table
+public class Student {
+    @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
+    private Long id;
+    private String name;
+
+    @Transient
+    private int age;
+
+    private LocalDate dob;
+    private String email;
+
+    public Student() {
+    }
+
+    public Student(Long id, String name, LocalDate dob, String email) {
+        this.id = id;
+        this.name = name;
+        this.dob = dob;
+        this.email = email;
+    }
+
+    public Student(String name, LocalDate dob, String email) {
+        this.name = name;
+        this.dob = dob;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return Period.between(this.dob, LocalDate.now()).getYears();
+    }
+
+    public LocalDate getDob() {
+        return dob;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", dob=" + dob +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,36 @@
+package com.example.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+import static java.time.Month.JANUARY;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository) {
+        return args -> {
+            Student mariam = new Student(
+                    "Mariam",
+                    LocalDate.of(2000, JANUARY, 5),
+                    "mariam.jamal@gmail.com"
+            );
+
+            Student alex = new Student(
+                    "Alex",
+                    LocalDate.of(2004, JANUARY, 5),
+                    "alex.jamal@gmail.com"
+            );
+
+            repository.saveAll(
+                    List.of(mariam, alex)
+            );
+        };
+    }
+}
